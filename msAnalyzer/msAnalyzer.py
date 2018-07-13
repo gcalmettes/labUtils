0dfbefd10848565cc01221311ac01dbb1630b6c1
gcalmettes <guillaume.calmettes@gmail.com>
2018-07-13 19:21:15 -0400

    added debounce function for better user experience when modifying tracer purity

--- msAnalyzer/msAnalyzer.py
+++ msAnalyzer/msAnalyzer.py
@@ -18,6 +18,28 @@ import scipy.stats as stats
 from scipy import optimize
 from multiprocessing import Pool
 
+
+# from https://gist.github.com/walkermatt/2871026
+from threading import Timer
+
+def debounce(wait):
+  """ Decorator that will postpone a functions
+      execution until after wait seconds
+      have elapsed since the last time it was invoked. """
+  def decorator(fn):
+    def debounced(*args, **kwargs):
+      def call_it():
+        fn(*args, **kwargs)
+      try:
+        debounced.t.cancel()
+      except(AttributeError):
+        pass
+      debounced.t = Timer(wait, call_it)
+      debounced.t.start()
+    return debounced
+  return decorator
+
+
 #############################################################################
 # --------- Natural Abundance Correction CLASS -----------------------------#
 #############################################################################
@@ -370,6 +392,7 @@ class MSDataContainer:
     print(f"The correction method for natural abundance has been updated to {newMethod}")
     self.computeNACorrectionDf()
 
+  @debounce(1.1) # make sure the function is not called over and over when textbox update call it
   def computeNACorrectionDf(self):
     self.dataDf_corrected = self.correctForNaturalAbundance()
     self.dataDf_labeledProportions = self.calculateLabeledProportionForAll()
