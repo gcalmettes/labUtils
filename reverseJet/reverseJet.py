df8be11238d982b8613c278ce2dec385086a03e8
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2018-10-02 18:21:13 -0700

    fixed wrong img url and added plots in main()

--- reverseJet/reverseJet.py
+++ reverseJet/reverseJet.py
@@ -21,11 +21,18 @@ def reverse_jet(img):
 
 if __name__ == '__main__':
 
-    out = reverse_jet('img2.tif')
+    url = 'im2.tif'
+    img = plt.imread(url)
+    out = reverse_jet(url)
 
     f, (ax0, ax1, ax2) = plt.subplots(1, 3)
-    ax0.imshow(img, cmap=plt.cm.gray)
-    ax1.imshow(out, cmap=plt.cm.gray)
-    ax2.imshow(out, cmap=plt.cm.jet)
+    ax0.imshow(img)
+    ax1.imshow(skimage.color.rgb2grey(img), cmap=plt.cm.gray)
+    ax2.imshow(out, cmap=plt.cm.gray)
+    ax0.set_title("original")
+    ax1.set_title("grayscale")
+    ax2.set_title("reversed jet")
+    for ax in [ax0, ax1, ax2]:
+      ax.axis('off')
 
     plt.show()
